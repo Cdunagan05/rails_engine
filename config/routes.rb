695776f1fd828ef5ea191def0b38082e3a4f7b43
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-10-04 13:41:30 -0600

    Going to combine migrations to one

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :invoice_items do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
+        get "/random", to: "search#random"
+      end
+      namespace :invoices do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
+        get "/random", to: "search#random"
+      end
+      namespace :items do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
+        get "/random", to: "search#random"
+      end
       resources :invoice_items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
